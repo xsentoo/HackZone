648864509cffac010fdbb84478275ba41abd66fc
senthooran <123653047+xsentoo@users.noreply.github.com>
2025-11-18 10:53:06 +0100

    first commit

---
+++ src/main/java/com/uphf/HackZone/HackZoneApplication.java
@@ -0,0 +1,13 @@
+package com.uphf.HackZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackZoneApplication.class, args);
+	}
+
+}
