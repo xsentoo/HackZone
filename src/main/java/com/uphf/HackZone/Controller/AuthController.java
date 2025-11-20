cdf3bf2e3ab236bdc7b9f993b66b2a2219b0c990
senthooran <123653047+xsentoo@users.noreply.github.com>
2025-11-20 19:40:58 +0100

    Ajout jwt mais pas encore a front

--- src/main/java/com/uphf/HackZone/Controller/AuthController.java
+++ src/main/java/com/uphf/HackZone/Controller/AuthController.java
@@ -1,8 +1,10 @@
 package com.uphf.HackZone.Controller;
 
 
+import com.uphf.HackZone.Component.JwtUtil;
 import com.uphf.HackZone.Entity.UserEntity;
 import com.uphf.HackZone.Repository.UserRepository;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +19,11 @@ import java.util.Optional;
 @RequestMapping("/Auth")
 public class AuthController {
     private final UserRepository userRepository;
+    private final JwtUtil JwtUtil;
 
-    public AuthController(UserRepository userRepository) {
+    public AuthController(UserRepository userRepository , JwtUtil jwtUtil) {
         this.userRepository = userRepository;
+        this.JwtUtil = jwtUtil;
     }
 
     @GetMapping("/login")
@@ -31,6 +35,8 @@ public class AuthController {
     public String LoginUser(@RequestParam String userMail ,@RequestParam String userPWD , Model model){
         Optional<UserEntity> user = userRepository.findByUserMail(userMail);
         if (user.isPresent() && user.get().getUserPWD().equals(userPWD)) {
+            String token = JwtUtil.generateToken(userMail);
+            model.addAttribute("token", token);
             model.addAttribute("user",user.get());
             return "Home";
         }
